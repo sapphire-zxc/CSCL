a487ee54cf80e608429461e906ce047214fc8cab
sapphire-zxc <izuee2166@gmail.com>
2024-08-14 10:44:16 +0200

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -89,7 +89,7 @@ export default {
                         <p class="type-label-md">Website layout made by <a href="https://tsl.pages.dev/" target="_blank">TheShittyList</a></p>
                     </div>
                     <template v-if="editors">
-                        <h3>List Editors</h3>
+                        <h3>        List Editors</h3>
                         <ol class="editors">
                             <li v-for="editor in editors">
                                 <img :src="\`/assets/\${roleIconMap[editor.role]}\${store.dark ? '-dark' : ''}.svg\`" :alt="editor.role">
